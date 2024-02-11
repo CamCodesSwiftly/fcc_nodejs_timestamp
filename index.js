525e1ddbeb55600ad076a2cca2ba0f4a09f77e6c
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-02-11 20:46:45 +0100

    feat: Handle milisecond and invalid input
    I check if the input string is an only integer input (which means it is miliseconds) and
    output the wished output json format.
    If the input string does not satisfy that condition, I output the wished error json output.
    
    From the current perspective this means 2/4 tasks handled.
    Empty must will be handled next. The most complicated is handling any kind of Date string that new Date(dateString) can
    handle. I'll do that as the last task.

--- index.js
+++ index.js
@@ -2,31 +2,51 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+	res.json({ greeting: "hello API" });
 });
 
+// handle date
+app.get("/api/:date", function (req, res) {
+  // any non-integer in the string?
+	for (const char of Array.from(req.params.date)) {
+		if (isNaN(parseInt(char, 10))) {
+			console.log(`${char} is not an integer.`);
+			return res.json({
+				error: "Invalid Date",
+			});
+		}
 
+		// When we reach this point, the string MUST be only integers
+		const miliseconds = parseInt(req.params.date, 10);
+		const date = new Date(miliseconds);
+		const utcDate = date.toUTCString();
+		console.log(date);
+		return res.json({
+			unix: miliseconds,
+			utc: utcDate,
+		});
+	}
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+	console.log("Your app is listening on port " + listener.address().port);
 });
